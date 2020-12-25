3f1fc87c00ee714191c6486efad27f489610c911
alice <aj9ms@virginia.edu>
2020-12-24 19:16:55 -0500

    Moved restocking JSON into data folder and created new userscripts folder with dice-a-roo QoL improvements script.

---
+++ userscripts/improve-dice-a-roo.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Dice-a-roo QoL Improvements
+// @namespace   Violentmonkey Scripts
+// @match       http://www.neopets.com/games/play_dicearoo.phtml
+// @grant       none
+// @version     1.0
+// @author      sangyx
+// @description Script runs when playing dice-a-roo.  Removes the "Stop playing and collect __ NP" button, and also widens the box above to fit more text on one line.
+// ==/UserScript==
+
+// Run program
+improveDiceARoo();
+
+function improveDiceARoo() {
+    const diceResult = $("table[align='center'][width='400']");
+    if (diceResult && diceResult[0]) {
+        diceResult[0]['width'] = 550;
+        diceResult[0].style.height = "80px";
+    }
+
+    const diceForms = $("form[action='play_dicearoo.phtml']");
+    if (diceForms && diceForms.length > 1 && diceForms[1]) {
+        diceForms[1].style.display = 'none';
+    }
+}
