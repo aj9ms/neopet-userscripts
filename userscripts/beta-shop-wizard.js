41b4f2c7437b60bc36fa00a3bd6c693212f463d0
alice <aj9ms@virginia.edu>
2020-12-31 04:37:27 -0500

    Added Speckled Negg to Foods shop.  Added new script for beta shop wizard (most only runs when typing paste keys - Ctrl+V)

---
+++ userscripts/beta-shop-wizard.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        Beta Shop Wizard Repeat Searching
+// @namespace   Violentmonkey Scripts
+// @match       http://www.neopets.com/shops/wizard.phtml
+// @grant       none
+// @version     1.0
+// @author      aj9ms
+// @description Script runs when on the shop wizard page.  Tries to retrieve copied text and pastes it into the search bar if available.  Changes dropdown from "Containing my phrase" to "Identical to."
+// ==/UserScript==
+
+// Run program
+shopWizSearch();
+document.addEventListener('paste', handlePaste);
+
+function shopWizSearch() { 
+    const dropdown = $('.wizard-filters > #criteria');
+
+    // The search results page has the same URL - if we're on that, we should ignore this script.
+    if (!dropdown) {
+        return;
+    }
+
+    // Change dropdown to "Identical to"
+    dropdown.val('exact');
+}
+
+function handlePaste (e) {
+    // The search results page has the same URL - if we're on that, we should ignore this script.
+    const wizard = $('#shopwizard');
+    if (!wizard) {
+        return;
+    }
+
+    // Stop data actually being pasted into div
+    e.stopPropagation();
+    e.preventDefault();
+
+    // Get pasted data via clipboard API
+    const clipboardData = e.clipboardData || window.clipboardData;
+    const pastedData = clipboardData.getData('Text');
+    
+    // Search
+    wizard.val(pastedData);
+    $('.button-search-white').click();
+}
